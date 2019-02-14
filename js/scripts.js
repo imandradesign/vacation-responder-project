e465f4f0ab281277899d8117a657834bbd9249c8
Imandra McKenzie <indigoimandra@gmail.com>
2019-02-13 17:48:59 -0800

    Remove form fields that aren't needed, fix scripts to properly show and hide content for different radio selections, adjust font size.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,55 @@
 $(document).ready(function() {
+  $("#reload").hide();
   $(".vacation-form form").submit(function(event) {
     var nameInput = $("input#name").val().charAt(0).toUpperCase() + $("input#name").val().slice(1);;
-    var streetAddressInput = $("input#street-address").val();
     var cityInput = $("input#city").val().charAt(0).toUpperCase() + $("input#city").val().slice(1);
     var stateInput = $("input#state").val().toUpperCase();
-    var zipCodeInput = $("input#zip-code").val();
     var season = $("#season").val();
     var favoriteColor = $("#color").val();
     var drink = $("input:radio[name=drink]:checked").val();
-    var consentInput = $("input#consent").val();
 
 
     $(".name").text(nameInput);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
 
-    if (true) {
-      $(".rome").show();
-    };
-
-    if (true) {
-      $(".hawaii").show();
-    };
-
-    if (true) {
-      $(".greenland").show();
-    };
 
     event.preventDefault();
-    });
 
-    $(".click-rome").click(function() {
-      $(".rome-facts").show();
-    });
+    $("form").hide();
+    $("#reload").show();
 
-    $(".click-hawaii").click(function() {
-      $(".hawaii-facts").show();
-    });
+    if (drink === 'mojito') {
+      $(".hawaii").show();
+    }
 
-    $(".click-greenland").click(function() {
-      $(".greenland-facts").show();
+    if (drink === "prosecco") {
+      $(".rome").show();
+    }
+
+    if (drink === "coffeewhiskey") {
+      $(".qaqortoq").show();
+    }
+  });
+
+  $(".click-rome").click(function() {
+    $(".rome-facts").show();
+  });
+
+  $(".click-hawaii").click(function() {
+    $(".hawaii-facts").show();
+  });
+
+  $(".click-qaqortoq").click(function() {
+    $(".qaqortoq-facts").show();
+  });
+
+  $("#reload").click(function(event) {
+    $("form").show();
+    $("#reload").hide();
+    $(".qaqortoq").hide();
+    $(".hawaii").hide();
+    $(".rome").hide();
+    event.preventDefault();
   });
 });
